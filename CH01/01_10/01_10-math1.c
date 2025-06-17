b69f4f5c43380cbc7a6444588ce4362983ad3b71
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-06-17 17:20:02 +0000

    updates

--- CH01/01_10/01_10-math1.c
+++ CH01/01_10/01_10-math1.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 /* The math header file contains definitions for M_PI
-   and the sin() function. It does not contain the
-   code for the sin() function. The code lies in the
-   math library file */
+	 and the sin() function. It does not contain the
+	 code for the sin() function. The code lies in the
+	 math library file */
 #include <math.h>
 
-int main()
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+		int
+		main()
 {
 	int w;
-	for( w=0; w<10; w++)
+	for (w = 0; w < 10; w++)
 	{
-		printf("%.2f\n",sin(M_PI/(w+1)) );
+		printf("%.2f\n", sin(M_PI / (w + 1)));
 	}
 	return 0;
 }
