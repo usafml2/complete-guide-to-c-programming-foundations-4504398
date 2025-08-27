b18ce7272082d8e6bc17bc0d6645c6ed46da8dd0
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-08-27 19:47:12 +0000

    updates

--- CH02/02_12/02_12-typecast1.c
+++ CH02/02_12/02_12-typecast1.c
@@ -7,7 +7,7 @@ int main()
 	a = 100;
 	b = 7;
 
-	printf("%d / %d = %d\n",a,b,a/b);
+	printf("%d / %d = %f\n",a,b,(float)a/b);
 
 	return(0);
 }
--- CH02/02_13/02_13-challenge1.c
+++ CH02/02_13/02_13-challenge1.c
@@ -3,19 +3,32 @@
 
 int main()
 {
-	//declare constant ratio equal to 3/4
-	//declare char a
-	//declare integer b
-	//declare float c
+	// declare constant ratio equal to 3/4
+	const float ratio = 3.0f / 4.0f;
 
-	//assign char
-	//assign integer
-	//assign float
+	// declare char a
+	char a;
+	// declare integer b
+	int b;
+	// declare float c
+	float c;
 
-	//output char value, e.g., "the value of variable a is '?'"
-	//output integer value, e.g., "the value of variable b is ??"
-	//output float value, e.g., "the value of variable c is ???.??"
-	//output value of ratio, e.g., "the value of constant ratio is ???.??"
+	// assign char
+	a = 'A';
+	// assign integer
+	b = 123;
+	// assign float
+	c = 123.456;
+
+	// output char value, e.g., "the value of variable a is '?'"
+	printf("The value of the variable a is '%c'\n", a);
+	// output integer value, e.g., "the value of variable b is ??"
+	printf("The value of the variable b is %d\n", b);
+
+	// output float value, e.g., "the value of variable c is ???.??"
+	printf("The value of the variable c is %.3f\n", c);
+	// output value of ratio, e.g., "the value of constant ratio is ???.??"
+	printf("The value of the constant ratio is %.2f\n", ratio);
 
 	return 0;
 }
