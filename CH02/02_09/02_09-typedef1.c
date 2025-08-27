35ce6987fb4ca4885d218574fc4a0e3fc1d92f94
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-08-27 17:34:07 +0000

    updates

--- CH02/02_09/02_09-typedef1.c
+++ CH02/02_09/02_09-typedef1.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
+typedef float radius;
+
 int main()
 {
-	typedef float radius;
 
 	radius a;
 
 	a = 27.5;
 
 	printf("A circle with radius %.2f has an area of  %.2f\n",
-			a,
-			a*a*3.1415926
-		  );
+				 a,
+				 a * a * 3.1415926);
 
-	return(0);
+	return (0);
 }
-
