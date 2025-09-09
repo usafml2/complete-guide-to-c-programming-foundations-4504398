2d0541bbaf8fc52dcfbe95e098e981955f53b228
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-09-09 15:38:29 +0000

    updates

--- CH03/03_02/03_02-chalenge1.c
+++ CH03/03_02/03_02-chalenge1.c
@@ -3,13 +3,18 @@
 int main()
 {
 	/* variable declarations */
-
+	int a, c;
+	float b;
 	/* variable assignments */
+	a = 15;
+	b = a / 2.0; // You have to divide by 2.0 to get
+	// a floating point result
+	c = b * 4;
 
 	/* output */
-	printf("Value of variable A = %d\n",a);
-	printf("Value of variable B = %f\n",b);
-	printf("value of vairable C = %d\n",c);
+	printf("Value of variable A = %d\n", a);
+	printf("Value of variable B = %f\n", b);
+	printf("value of vairable C = %d\n", c);
 
-	return(0);
+	return (0);
 }
