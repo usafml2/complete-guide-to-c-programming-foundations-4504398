dc878c1380ef6301e36e6e69e9a11e52fc6dc4b6
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-09-18 14:02:46 +0000

    updates

--- CH03/03_04/03_04-assignment1.c
+++ CH03/03_04/03_04-assignment1.c
@@ -4,15 +4,15 @@ int main()
 {
 	int a = 25;
 
-	printf("The value of a is %d\n",a);
-	a = a + 5;
-	printf("The value of a is %d\n",a);
-	a = a - 5;
-	printf("The value of a is %d\n",a);
-	a = a * 5;
-	printf("The value of a is %d\n",a);
-	a = a / 5;
-	printf("The value of a is %d\n",a);
+	printf("The value of a is %d\n", a);
+	a += 5;
+	printf("The value of a is %d\n", a);
+	a -= 5;
+	printf("The value of a is %d\n", a);
+	a *= 5;
+	printf("The value of a is %d\n", a);
+	a /= 5;
+	printf("The value of a is %d\n", a);
 
-	return(0);
+	return (0);
 }
