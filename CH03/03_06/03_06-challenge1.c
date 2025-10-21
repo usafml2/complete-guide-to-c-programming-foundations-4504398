a319830a73c8e241ecdb7f9ad89b10d898e0c5ca
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-10-21 13:23:55 +0000

    updates

--- CH03/03_06/03_06-challenge1.c
+++ CH03/03_06/03_06-challenge1.c
@@ -4,7 +4,12 @@ int main()
 {
 	int artifacts,rooms,paintings,sculptures;
 
+	rooms = 12;
+	paintings = 16;
+	sculptures = 4;
+
 	/* code your calculations here */
+	artifacts = rooms * (paintings + sculptures);
 
 	printf("This museum has %d artifacts\n",artifacts);
 
