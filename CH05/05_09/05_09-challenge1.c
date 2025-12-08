588e6fe8652a12a5b9cbca4841110944c65f6a2d
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-12-08 16:21:53 +0000

    updates

--- CH05/05_09/05_09-challenge1.c
+++ CH05/05_09/05_09-challenge1.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+void line(int n, char ch)
+{
+	for (int i = 0 ; i < n; i++)
+	   putchar(ch);
+
+putchar('\n');
+}
+
 int main()
 {
 	puts("Report on Extraterrestrial Activity");
 	line(35,'-');
+	line(20,'$');
 
 	return(0);
 }
