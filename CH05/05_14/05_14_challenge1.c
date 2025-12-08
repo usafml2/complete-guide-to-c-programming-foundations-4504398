94d062b4ae821230ef6345b35fbe2d953f82e9ff
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2025-12-08 21:31:17 +0000

    updates

--- CH05/05_14/05_14_challenge1.c
+++ CH05/05_14/05_14_challenge1.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 
+int add (int a, int b)
+{
+	printf("%d + %d = %d\n",a,b,a+b);
+	return 0;
+}
+int subtract (int a, int b)
+{
+	printf("%d - %d = %d\n",a,b,a-b);
+	return 0;
+}
+int multiply (int a, int b)
+{
+	printf("%d * %d = %d\n",a,b,a*b);
+	return 0;
+}
+float divide (int a, int b)
+{
+	return (float)a / (float)b;
+}
+
+
+
 int main()
 {
 	int a,b;
 
-	printf("Enter two values, separated by a space: ");
+	printf("Enter two whole numbers, separated by a space: ");
 	scanf("%d",&a);
 	scanf("%d",&b);
 
