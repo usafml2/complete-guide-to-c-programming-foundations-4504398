98078e0d1bc07d5fbe2eafc3de69818d0bf1ac9b
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-01-12 13:49:36 +0000

    updates

--- CH06/06_02/06_02-highscores1.c
+++ CH06/06_02/06_02-highscores1.c
@@ -3,6 +3,7 @@
 int main()
 {
 	float highscore[5];
+	int x;
 
 	highscore[0] = 993.86;
 	highscore[1] = 682.01;
@@ -11,12 +12,8 @@ int main()
 	highscore[4] = 123.25;
 
 	puts("Here are the high scores:");
-	printf("#1 %.2f\n",highscore[0]);
-	printf("#2 %.2f\n",highscore[1]);
-	printf("#3 %.2f\n",highscore[2]);
-	printf("#4 %.2f\n",highscore[3]);
-	printf("#5 %.2f\n",highscore[4]);
+	for (x = 0; x < 5; x++)
+		printf("#%d %.2f\n", x + 1, highscore[x]); // Print first high score
 
-	return(0);
+	return (0);
 }
-
--- CH06/06_03/06_03-challenge1.c
+++ CH06/06_03/06_03-challenge1.c
@@ -2,5 +2,7 @@
 
 int main()
 {
+	int ages[] = {23, 45, 12, 67, 34};
+	int *ptr = ages; // Pointer to the first element of the array
 	return 0;
 }
