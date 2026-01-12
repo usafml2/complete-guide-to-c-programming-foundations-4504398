c833c835926551db8e5e97dc14fd369009dd9f58
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-01-12 20:15:41 +0000

    updates

--- CH06/06_09/06_09-challenge1.c
+++ CH06/06_09/06_09-challenge1.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+struct person
+{
+	char name[50];
+	float height;
+	struct date
+	{
+		int month;
+		int day;
+		int year;
+	} birthday;
+};
+
 int main()
 {
+	struct person individual = {
+			"Chuck Watkins",
+			 0.0018161,
+			 {9, 10, 1956}};
+	printf("name: %s, height: %.7f km, birthday: %d/%d/%d\n",
+		individual.name,
+		individual.height,
+		individual.birthday.month,
+		individual.birthday.day,
+		individual.birthday.year
+	);
 	return 0;
 }
