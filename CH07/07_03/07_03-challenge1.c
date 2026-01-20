a958663d32692cde6c27d649fd7cfbd28513a487
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-01-20 22:12:47 +0000

    My solution

--- CH07/07_03/07_03-challenge1.c
+++ CH07/07_03/07_03-challenge1.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main()
 {
 	char string[] = "Nifty TEXT! 123\n";
+	char ch;
+	int x = 0;
 
-	return(0);
+	while ((ch = string[x]) != '\0')
+	{
+		if (isupper(ch))
+			putchar(tolower(ch));
+		else if (islower(ch))
+			putchar(toupper(ch));
+		else putchar(ch);
+		x++;
+	}
+
+	return (0);
 }
+
+	
