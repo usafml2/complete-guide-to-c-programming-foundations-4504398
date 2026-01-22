6b78d031e64a36784fa340df8236bd31b06c606a
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-01-22 15:23:30 +0000

    Added comments

--- CH07/07_06/07_06-yourname3.c
+++ CH07/07_06/07_06-yourname3.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+#include <stdio.h> // Include standard input/output library.
 
 int main()
 {
-	char input[10];
-	int i;
+	char input[10]; // Array to hold user input.
+	int i; // Loop counter.
 
 	printf("Your name? ");
 	fgets(input,10,stdin);
-	for(i=0;i<10;i++)
+	for(i=0;i<10;i++) // Loop to find newline character.
+	// Post increment instead of preincrement is
+	// used since we want to start checking from index 0.
 	{
-		if(input[i] == '\n')
-			input[i] = '\0';
+		if(input[i] == '\n') // If newline found,
+			input[i] = '\0'; // replace with null terminator.
 	}
-	printf("Pleased to meet you, %s!\n",input);
+	printf("Pleased to meet you, %s!\n",input); // Greet the user. \n is newline character.
 
-	return(0);
+	return(0); // Return 0 to indicate successful completion.
 }
 
