c3aee2ab7c61e2fb47332a999777686f72d0810e
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-01-22 20:04:56 +0000

    Added program

--- CH07/07_08/07_08-challenge1.c
+++ CH07/07_08/07_08-challenge1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-	return 0;
+	int quantity = 12;
+	char item[] = "fish umbrellas";
+	float price = 78.66f;
+	float total = quantity * price;
+
+	printf("You have purchased %d %s at $%.2f each.\nThe total cost is %.2f\n", quantity, item, price, total);
+
+			return 0;
 }
