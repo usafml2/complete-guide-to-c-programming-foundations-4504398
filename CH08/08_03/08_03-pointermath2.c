ea754c2226717e616ea9e1c325aee9e009a356d8
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-11 19:40:00 +0000

    Added more comments

--- CH08/08_03/08_03-pointermath2.c
+++ CH08/08_03/08_03-pointermath2.c
@@ -9,7 +9,9 @@ int main()
 	// no ampersand is needed because the name of an array
 	// is a pointer to its first element
 	printf("%p\n",pt);
-	printf("%p\n",pt+1);
+	printf("%p\n",pt+1); // pt+1 points to the second element of twos
+	// pointer arithmetic takes into account the size of the data type
+	// so pt+1 is actually 4 bytes (the size of an int) beyond pt
 
 	return(0);
 }
