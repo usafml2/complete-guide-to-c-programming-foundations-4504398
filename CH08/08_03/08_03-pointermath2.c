0a88bad629d59c7746a325dbbcf5b37be646fec7
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-11 19:38:16 +0000

    Added comments

--- CH08/08_03/08_03-pointermath2.c
+++ CH08/08_03/08_03-pointermath2.c
@@ -5,7 +5,9 @@ int main()
 	int twos[5] = { 2, 4, 6, 8, 10 };
 	int *pt;
 
-	pt = twos;
+	pt = twos; // pt points to the first element of twos
+	// no ampersand is needed because the name of an array
+	// is a pointer to its first element
 	printf("%p\n",pt);
 	printf("%p\n",pt+1);
 
