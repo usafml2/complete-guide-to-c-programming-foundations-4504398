d7ad334c5cc409200090acc1e34e93c773a1a87b
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-16 16:12:46 +0000

    Added code with AI help

--- CH08/08_05/08_05-challenge1.c
+++ CH08/08_05/08_05-challenge1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	char title[] = "Pointers don't intimidate me!\n";
+	char *pt;
+	int x;
+
+	pt = title;
+	for (x = 0; x < (int)strlen(title); x++)
+	{
+		printf("%c", *(pt + x));
+	}
 
 	return 0;
 }
