43359c81e5784bd42c44564a8a722d47ff7dca80
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-16 16:43:03 +0000

    Added test code for change in pt

--- CH08/08_05/08_05-challenge1.c
+++ CH08/08_05/08_05-challenge1.c
@@ -8,10 +8,12 @@ int main()
 	int x;
 
 	pt = title;
+	//printf("The address of pt is %p\n", pt);
 	for (x = 0; x < (int)strlen(title); x++)
 	{
 		printf("%c", *(pt + x));
 	}
-
+  //printf("The address of pt is %p\n", pt);
+	//The address of pt has not changed.
 	return 0;
 }
