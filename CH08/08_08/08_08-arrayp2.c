1b9be577605ca636e739e6223e169c199d012003
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 14:27:53 +0000

    Added comments

--- CH08/08_08/08_08-arrayp2.c
+++ CH08/08_08/08_08-arrayp2.c
@@ -6,7 +6,9 @@ int main()
 	int x;
 
 	for(x=0;x<5;x++)
-		printf("%s\n",*(veeps+x));
+		printf("%s\n",*(veeps+x)); // veeps is an array of pointers to char,
+		// so veeps+x gives us a pointer to the x-th element of the array,
+		// which is a pointer to char. Then we dereference that pointer to get the string it points to.
 
 	return(0);
 }
