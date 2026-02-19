42ae0de231588f4d844139bc985c1507f0b1ec9e
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 14:44:58 +0000

    Added more comments

--- CH08/08_08/08_08-arrayp1.c
+++ CH08/08_08/08_08-arrayp1.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-	char *veeps[5] = { "Adams", "Jefferson", "Burr", "Clinton", "Gerry" }; // array of 5 pointers to char
+	char *veeps[5] = { "Adams", "Jefferson", "Burr", "Clinton", "Gerry" }; // array of 5 pointers
+	// to character strings (array of strings)
 	int x;
 
 	for(x=0;x<5;x++)
-		printf("%s\n",veeps[x]); // print the string pointed to by each element of the array. veeps[x] instead
-		// of *veeps[x] because veeps[x] is already a pointer to char, so we can use it directly in printf.
+		printf("%s\n",veeps[x]); // print the string pointed to
+		// by each element of the array. veeps[x] instead
+		// of *veeps[x] because veeps[x] is already a pointer to char,
+		// so we can use it directly in printf.
 
 	return(0);
 }
