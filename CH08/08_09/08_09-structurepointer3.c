3fa46b118b7a420c3ff1ac45d5fd52f5ee632c22
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 16:04:07 +0000

    Comments added

--- CH08/08_09/08_09-structurepointer3.c
+++ CH08/08_09/08_09-structurepointer3.c
@@ -5,9 +5,9 @@
 int main()
 {
 	struct person {
-		char *name;
+		char *name; // name is a pointer to char (string)
 		int age;
-	} *you;
+	} *you; // you is a pointer to a structure variable of type struct person
 
 	/* allocate storage */
 	you = malloc( sizeof(struct person) );
@@ -16,7 +16,9 @@ int main()
 		puts("Unable to allocate storage");
 		exit(1);
 	}
-	you->name = malloc( sizeof(char) * 32 );
+	you->name = malloc( sizeof(char) * 32 ); // allocate memory for 32 characters to hold the name
+	// Note: you->name is equivalent to (*you).name,
+	// which accesses the name member of the structure pointed to by you.
 	if( you->name == NULL)
 	{
 		puts("Unable to allocate storage");
@@ -25,9 +27,16 @@ int main()
 
 	/* gather input */
 	printf("Enter your name: ");
-	fgets(you->name,32,stdin);
+	fgets(you->name,32,stdin); // read a line of input into you->name,
+	// with a maximum of 31 characters (plus null terminator)
+	// Note: you->name is equivalent to (*you).name, 
+	// which accesses the name member of the structure pointed to by you.
 	printf("Enter your age: ");
-	scanf("%d",&you->age);
+	scanf("%d",&you->age); // read an integer from input and store it in you->age.
+	// Note: &you->age is the address of you->age, which is needed
+	// for scanf to store the input value.
+	// you->age is equivalent to (*you).age, which accesses the age member of
+	// the structure pointed to by you.
 
 	printf("You are %s",you->name);
 	printf("You are %d years old\n",you->age);
