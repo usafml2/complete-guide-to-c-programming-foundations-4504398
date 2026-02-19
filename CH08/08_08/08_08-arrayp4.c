e8a5476ab23b11e4ea9dc10956acdba9a9bf817f
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 14:33:39 +0000

    Added comments

--- CH08/08_08/08_08-arrayp4.c
+++ CH08/08_08/08_08-arrayp4.c
@@ -6,7 +6,11 @@ int main()
 	int x;
 
 	for(x=0;x<5;x++)
-		printf("%c\n",**(veeps+x));
+		printf("%c\n",**(veeps+x)); // veeps is an array of pointers to char,
+		// so veeps+x gives us a pointer to the x-th element of the array,
+		// which is a pointer to char. Then we dereference that pointer 
+		//to get the string it points to, and then we dereference that string
+		//to get the first character of the string.
 
 	return(0);
 }
