a1df14cd6abb5fa3509ab0384fec903e6e43aa59
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 15:45:52 +0000

    Added more comments

--- CH08/08_09/08_09-structurepointer1.c
+++ CH08/08_09/08_09-structurepointer1.c
@@ -10,7 +10,7 @@ int main()
 	} you; // you is a structure variable of type struct person
 
 	/* allocate storage */
-	you.name = malloc( sizeof(char) * 32);
+	you.name = malloc( sizeof(char) * 32); // allocate memory for 32 characters to hold the name
 	if( you.name == NULL)
 	{
 		puts("Unable to allocate storage");
@@ -19,9 +19,11 @@ int main()
 
 	/* gather input */
 	printf("Enter your name: ");
-	fgets(you.name,32,stdin);
+	fgets(you.name,32,stdin); // read a line of input into you.name,
+	// with a maximum of 31 characters (plus null terminator)
 	printf("Enter your age: ");
-	scanf("%d",&you.age);
+	scanf("%d",&you.age); // read an integer from input and store it in you.age.
+	// Note: &you.age is the address of you.age, which is needed for scanf to store the input value.
 
 	printf("You are %s",you.name);
 	printf("You are %d years old\n",you.age);
