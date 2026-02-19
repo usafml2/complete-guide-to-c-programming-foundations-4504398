43eaffb104b0dab7b4be60119d90b07544e79293
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 20:51:36 +0000

    Dded comments

--- CH08/08_12/08_12-passpointer1.c
+++ CH08/08_12/08_12-passpointer1.c
@@ -11,7 +11,7 @@ int main()
 	int a = 25;
 
 	printf("Value before: %d\n",a);
-	doubler(&a); // Pass the address of a to the function,
+	doubler(&a); // Pass the address of of variable a to the function,
 	// so that it can modify the value of a
 	printf("Value after: %d\n",a);
 
--- CH08/08_12/08_12-passpointer2.c
+++ CH08/08_12/08_12-passpointer2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-void three(float a, float *b, float *c)
+void three(float a, float *b, float *c) // The parameters b and c are pointers to floats
 {
-	*b = a/3.0;
-	*c = a/7.0;
+	*b = a/3.0; // * dereferences the pointer b to access the value
+	// of the variable that is passed to the function as an argument,
+	// and assigns a new value to it.
+	*c = a/7.0; // The function can modify the values of b and c,
+	// which are passed by reference, by dereferencing the pointers
+	// and assigning new values to them.
 }
 
 int main()
