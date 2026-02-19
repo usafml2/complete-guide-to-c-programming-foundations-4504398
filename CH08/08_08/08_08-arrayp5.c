0cec18bfd11b09037aefb50ad04fb32faf714434
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 14:49:45 +0000

    Added comments

--- CH08/08_08/08_08-arrayp5.c
+++ CH08/08_08/08_08-arrayp5.c
@@ -9,7 +9,11 @@ int main()
 		printf("%c\n",*(*(veeps+x)+1)); // prints the second letter of each name
 		// the above statement is equivalent to:
 		// printf("%c\n",veeps[x][1]);
-		 
+		// *(veeps+x) gives us a pointer to the x-th element of the array,
+		// which is a pointer to char (a string). Then we add 1 to that pointer
+		// to get a pointer to the second character of the string,
+		// and then we dereference that pointer to get the character itself. 
+		
 	return(0);
 }
 
