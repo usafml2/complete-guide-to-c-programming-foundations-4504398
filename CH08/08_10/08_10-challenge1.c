4e69e49d00706017c2e59d36089f6b121b366b03
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 18:26:33 +0000

    Comments added

--- CH08/08_10/08_10-challenge1.c
+++ CH08/08_10/08_10-challenge1.c
@@ -1,7 +1,24 @@
+// Challenge: Create an array of structure pointers
+// Create a structure that contains an integer and a character.
+// The array contains 26 elements, each representing a letter of the alphabet.
+// First structure: int value 0, char value 'A'
+// Second structure: int value 25, char value 'Z'
+// Then create an array of pointers to this structure,
+// and allocate memory for each pointer in the array.
+// Finally, assign values to the members of each structure
+// through the pointers and output the data stored in each structure.
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
+	struct firstStructure
+	{
+		int value;
+		char letter;
+	}
+
 	return 0;
 }
