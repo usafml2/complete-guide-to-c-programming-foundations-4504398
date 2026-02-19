9543823873dc4e7c3994401adcf917e15c371224
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-02-19 20:32:09 +0000

    Added comments

--- CH08/08_12/08_12-passpointer1.c
+++ CH08/08_12/08_12-passpointer1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-void doubler(int *alpha)
+void doubler(int *alpha) // The parameter alpha is a pointer to an integer
+// 
 {
-	*alpha += *alpha;
+	*alpha += *alpha; // *alpha = *alpha + *alpha
 }
 
 int main()
@@ -10,7 +11,8 @@ int main()
 	int a = 25;
 
 	printf("Value before: %d\n",a);
-	doubler(&a);
+	doubler(&a); // Pass the address of a to the function,
+	// so that it can modify the value of a
 	printf("Value after: %d\n",a);
 
 	return(0);
