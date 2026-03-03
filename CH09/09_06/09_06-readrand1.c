a43ad972028210b2cd79c8c7da8545c24968c759
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-03-03 16:37:30 +0000

    Added comments

--- CH09/09_06/09_06-readrand1.c
+++ CH09/09_06/09_06-readrand1.c
@@ -2,17 +2,21 @@
 
 int main()
 {
-	const char filename[] = "presidents.dat";
-	struct person {
+	const char filename[] = "presidents.dat"; // name of the file to read. 
+	struct person { // define a structure to hold the data for each president.
 		char name[32];
 		int inauguration;
 		int age;
-	} president;
-	int x;
-	FILE *fp;
+	} president; // declare a variable of type struct person to hold
+	// the data read from the file.
+	int x; // declare an integer variable to hold the record number to read.
+	FILE *fp; // declare a file pointer to hold the address of the file to be read.
+	// FILE is a type defined in stdio.h that represents a file stream.
 
 	/* open the file */
-	fp = fopen(filename,"r");
+	fp = fopen(filename,"r"); // open the file for reading.
+	// The "r" mode indicates that the file is opened for
+	// reading only.
 	if( fp == NULL )
 	{
 		printf("Unable to open %s\n",filename);
@@ -21,20 +25,37 @@ int main()
 
 	/* prompt to read a specific record */
 	printf("Enter record to read (1-10): ");
-	scanf("%d",&x);
+	scanf("%d",&x); // read an integer from the user
+	// and store it in x. &x is the address of x,
+	// which is passed to scanf so that it can
+	// modify the value of x.
 	/* validate the input */
-	if( x<1 || x>10 )
+	if( x<1 || x>10 ) // check if the input(record number)
+	// is within the valid range of 1 to 10
 	{
 		puts("Invalid input");
 		return(1);
 	}
 	/* decrement x to reference the proper offset */
-	x--;
+	x--; // decrement x to convert from 1-based to 0-based indexing
 
 	/* Set the file position indicator */
-	fseek(fp, sizeof(struct person)*x, SEEK_SET);
+	fseek(fp, sizeof(struct person)*x, SEEK_SET); // move the file
+	// position indicator to the
+	// position of the x-th record
+	// in the file. fp is the file pointer,
+	// sizeof(struct person)*x
+	// calculates the byte offset of the x-th record,
+	// and SEEK_SET indicates that the offset
+	// is from the beginning of the file.
+
 	/* fetch the record */
-	fread(&president, sizeof(struct person), 1, fp);
+	fread(&president, sizeof(struct person), 1, fp); // read one record of size
+	// sizeof(struct person) from the file
+	// and store it in the variable president.
+	// &president is the address of the president
+	// fp is the file pointer, and 1 indicates that
+	// we want to read one record.
 
 	/* print the result */
 	printf("President %s was %d years old when inaugurated in %d\n",
