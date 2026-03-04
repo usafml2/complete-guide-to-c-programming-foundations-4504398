9779ea037595a7199a7bb37dcef5a2b40642eea3
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-03-04 13:35:06 +0000

    Added comments

--- CH09/09_08/09_08-solution2.c
+++ CH09/09_08/09_08-solution2.c
@@ -2,14 +2,21 @@
 
 int main()
 {
-	const char filename[] = "presidents.dat";
-	struct person {
+	const char filename[] = "presidents.dat"; // define the name of the file to read from
+	// .dat is a binary file that contains the data for each president
+	// filename[] is a constant character array that holds the name of the file to read from
+	struct person { // define a structure to hold the data for each president
 		char name[32];
 		int inauguration;
 		int age;
-	} president;
+	} president; // create a variable of type struct person to hold the data
+	// for each president
 	int x;
-	FILE *fp;
+	FILE *fp; // create a file pointer to hold the address of the file we will open.
+	// FILE is a structure and data type that holds information about a file,
+	// such as its name, size, and position in the file.
+	// The *fp is a pointer to a FILE structure,
+	// which allows us to access the file's contents.
 
 	/* open the file */
 	fp = fopen(filename,"r");
@@ -22,8 +29,18 @@ int main()
 	/* read records from back to front */
 	for( x=-1; x>-11; x-- )
 	{
-		fseek(fp, sizeof(struct person)*x, SEEK_END);
-		fread(&president, sizeof(struct person), 1, fp);
+		fseek(fp, sizeof(struct person)*x, SEEK_END); // move the file pointer to the correct position
+		// read the record into the structure from the file.
+		// SEEK_END is used to move the file pointer to the end of the file,
+		// and then we use fseek to move it backwards
+		// by the size of the structure multiplied by x (which is negative).
+		// This allows us to read the last 10 records in reverse order.
+
+		fread(&president, sizeof(struct person), 1, fp); // read the record into
+		// the structure from the file. 1 is the number of records to read,
+		// and sizeof(struct person) is the size of each record.
+		//fp is the file pointer to the file we opened earlier.
+
 		/* print the result */
 		printf("President %s was %d years old when inaugurated in %d\n",
 				president.name,
