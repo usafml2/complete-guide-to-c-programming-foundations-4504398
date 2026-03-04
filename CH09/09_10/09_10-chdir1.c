3b9da0e02f4d81e2e6517eda41fe7a9ffeaaf1f4
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-03-04 14:59:13 +0000

    Added comments

--- CH09/09_10/09_10-chdir1.c
+++ CH09/09_10/09_10-chdir1.c
@@ -3,10 +3,13 @@
 
 int main()
 {
-	char path[512];
+	char path[512]; // buffer to hold the current
+	// working directory path.
 
-	chdir("..");
-	getcwd(path,512);
+	chdir(".."); // change the current working
+	// directory to the parent directory.
+	getcwd(path,512); // get the current working directory path.
+	
 	printf("This program is running in the %s directory\n",path);
 
 	return(0);
