f77d0f9a2b6368d9fbfa382f76eac500dc74ad80
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-03-05 14:07:09 +0000

    Added comments

--- CH09/09_11/09_11-readdir1.c
+++ CH09/09_11/09_11-readdir1.c
@@ -3,10 +3,17 @@
 
 int main()
 {
-	DIR *directory;
+	DIR *directory; // pointer to directory stream.
+	// DIR is a structure type defined in dirent.h.
+	// It is used to represent a directory stream.
+	// FILE is a structure type defined in stdio.h.
+	// It is used to represent a file stream.
 
 	/* open the directory */
-	directory = opendir(".");
+	directory = opendir("."); // open current directory
+	// . is a special directory name that refers to the
+	//  current directory. Syntax of opendir() is similar
+	// to fopen().
 	if( directory == NULL )
 	{
 		puts("Unable to open current directory");
@@ -14,10 +21,10 @@ int main()
 	}
 
 	/* do something */
-	puts("Opened current directory");
+	puts("Opened current directory"); // do something with the directory stream
 
 	/* close the directory */
-	closedir(directory);
+	closedir(directory); // close the directory stream
 
 	return(0);
 }
