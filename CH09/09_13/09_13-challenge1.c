589a4a24ed81a7e9a9ed824ca876475c3783eb01
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-03-05 19:03:28 +0000

    Added code

--- CH09/09_13/09_13-challenge1.c
+++ CH09/09_13/09_13-challenge1.c
@@ -15,14 +15,39 @@ void create_file(const char *f)
 
 	/* output */
 	fprintf(fp, "Chuck Watkins\n");
+	puts("The file has been written");
 
 	/* close the file */
 	fclose(fp);
+	puts("The file has been closed");
 }
 
-//void read_file(const char *f)
-//{
-//}
+void read_file(const char *f)
+{
+	FILE *fh;
+	int ch;
+
+	/* open the file */
+	fh = fopen(f, "r");
+	if (fh == NULL)
+	{
+		printf("Unable to read from file %s\n", f);
+	}
+  puts("The file output is ");
+
+	while (1)
+	{
+		ch = fgetc(fh);
+		if (ch == EOF)
+			break;
+		putchar(ch);
+		
+	}
+  
+	/* clean-up */
+	fclose(fh);
+	
+}
 
 //void delete_file(const char *f)
 //{
@@ -33,7 +58,7 @@ int main()
 	const char filename[] = "stuff.dat";
 
 	create_file(filename);
-	//read_file(filename);
+	read_file(filename);
 	//delete_file(filename);
 
 	return 0;
