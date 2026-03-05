3dce6f8e024e393b24bd61b60092fea772a8968a
Chuck Watkins <122413005+usafml2@users.noreply.github.com>
2026-03-05 14:54:26 +0000

    Added comments

--- CH09/09_11/09_11-readdir3.c
+++ CH09/09_11/09_11-readdir3.c
@@ -15,8 +15,17 @@ int main()
 	}
 
 	/* do something */
-	while( (file=readdir(directory)) != NULL )
-		printf("Found the file %s\n",file->d_name);
+	while( (file=readdir(directory)) != NULL ) // read each entry
+	// in the directory stream.
+	// The readdir() function returns a pointer to a dirent
+	// structure representing the next directory entry
+	// in the directory stream. If there are no more entries,
+	// it returns NULL. file is a pointer to a dirent structure
+	// that contains information about the directory entry,
+	// including the name of the file or directory (d_name).
+
+		printf("Found the file %s\n",file->d_name); // print the name
+		// of the file or directory. 
 
 	/* close the directory */
 	closedir(directory);
